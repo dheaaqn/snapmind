acecd53d54b7c6f54dd0360bd60700c385219e43
Dhea Qonita Irawan <dheaaqn@gmail.com>
2024-12-06 22:58:38 +0700

    initialize project

---
+++ src/navigation/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Home } from '../screens';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigator = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={Home} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default AppNavigator;
